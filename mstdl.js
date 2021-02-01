04b11e36873bf7de03b524ffd5b979997e305b3d
Dreller <60242965+Dreller@users.noreply.github.com>
2021-02-01 12:12:22 -0500

    WIP
    First save of WIP.

---
+++ mstdl.js
@@ -0,0 +1,9 @@
+function buildLink(strFormID){
+    var myForm = document.getElementById(strFormID);
+
+    
+    var linkLabel = document.getElementById('chatLinkLabel').value;
+    
+    alert(linkLabel);
+
+}
