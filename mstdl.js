06880a11cf8ad19566f1be98eb2eedeae7cd6539
Dreller <60242965+Dreller@users.noreply.github.com>
2021-02-01 13:14:11 -0500

    Working base
    First working base.

--- mstdl.js
+++ mstdl.js
@@ -3,7 +3,28 @@ function buildLink(strFormID){
 
     
     var linkLabel = document.getElementById('chatLinkLabel').value;
+    var linkIcon = document.getElementById('chatTeamsIcon').value;
+
+    var addrs = document.getElementById('chatAddresses').value;
+    var addr = addrs.replace("\n", ',');
+
+    var subject = document.getElementById('chatSubject').value;
+    var message = document.getElementById('chatMessage').value;
     
-    alert(linkLabel);
+    var myLink = "https://teams.microsoft.com/l/chat/0/0?";
+    myLink += "users=" + addr;
+    
+    if( subject != "" ){
+        myLink += "&topicName=" + subject;
+    }
+
+    if( message != "" ){
+        myLink += "&message=" + message;
+    }
+
+    var finalLink = encodeURI(myLink);
+    document.getElementById("generatedLink").value = finalLink;
+    document.getElementById("generatedLinkHelp").innerHTML = "Use this URL to build your link!";
+
 
 }
